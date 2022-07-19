d84e5b4e630bbee6a3b1f6a49cb9fa26faa9cc09
Giovanni Marcatto <gzmarcatto@gmail.com>
2022-07-19 14:50:06 -0300

    teste

--- src/Corpo-sidebar-sugestoes.js
+++ src/Corpo-sidebar-sugestoes.js
@@ -1,4 +1,6 @@
-export default function CorpoSidebarSugestoes(){
+import Sugestao from "./Corpo-sidebar-sugestoes-props"
+
+export default function CorpoSidebarSugestoes(props){
   return(
     <div class="sugestoes">
           <div class="titulo">
@@ -6,76 +8,19 @@ export default function CorpoSidebarSugestoes(){
             <div>Ver tudo</div>
           </div>
 
-          <div class="sugestao">
-            <div class="usuario">
-              <img src="assets/img/bad.vibes.memes.svg" />
-              <div class="texto">
-                <div class="nome"></div>
-                <div class="razao"></div>
-              </div>
-            </div>
-
-            <div class="seguir">Seguir</div>
-          </div>
-          <div class="sugestao">
-              <div class="usuario">
-                <img src="assets/img/bad.vibes.memes.svg" />
-                <div class="texto">
-                  <div class="nome">bad.vibes.memes</div>
-                  <div class="razao">Segue você</div>
-                </div>
-              </div>
-
-              <div class="seguir">Seguir</div>
-            </div>
-
-            <div class="sugestao">
-              <div class="usuario">
-                <img src="assets/img/chibirdart.svg" />
-                <div class="texto">
-                  <div class="nome">chibirdart</div>
-                  <div class="razao">Segue você</div>
-                </div>
-              </div>
-
-              <div class="seguir">Seguir</div>
-            </div>
-
-            <div class="sugestao">
-              <div class="usuario">
-                <img src="assets/img/razoesparaacreditar.svg" />
-                <div class="texto">
-                  <div class="nome">razoesparaacreditar</div>
-                  <div class="razao">Novo no Instagram</div>
-                </div>
-              </div>
-
-              <div class="seguir">Seguir</div>
-            </div>
-
-            <div class="sugestao">
-              <div class="usuario">
-                <img src="assets/img/adorable_animals.svg" />
-                <div class="texto">
-                  <div class="nome">adorable_animals</div>
-                  <div class="razao">Segue você</div>
-                </div>
-              </div>
-
-              <div class="seguir">Seguir</div>
-            </div>
-
-            <div class="sugestao">
-              <div class="usuario">
-                <img src="assets/img/smallcutecats.svg" />
-                <div class="texto">
-                  <div class="nome">smallcutecats</div>
-                  <div class="razao">Segue você</div>
-                </div>
-              </div>
-
-              <div class="seguir">Seguir</div>
-            </div>
+        <Sugestao src="assets/img/bad.vibes.memes.svg" name="bad.vibes.memes" razao="Segue você" />
+        <Sugestao src="assets/img/chibirdart.svg" name="chibirdart" razao="Segue você" />
+        <Sugestao
+          src="assets/img/razoesparaacreditar.svg"
+          name="razoesparaacreditar"
+          razao="Novo no Instagram"
+        />
+        <Sugestao
+          src="assets/img/adorable_animals.svg"
+          name="adorable_animals"
+          razao="Segue você"
+        />
+        <Sugestao src="assets/img/smallcutecats.svg" name="smallcutecats" razao="Segue você" />
 
           <div class="links">
             Sobre • Ajuda • Imprensa • API • Carreiras • Privacidade • Termos • Localizações •
